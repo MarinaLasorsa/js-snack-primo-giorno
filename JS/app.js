51e2c686f32b0bdb7c078d377f939496a802c2cc
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-03-01 15:22:56 +0100

    initial commit + Exercise 1

---
+++ JS/app.js
@@ -0,0 +1,33 @@
+//ESERCIZIO 1
+//Dichiara una variabile chiamata number e assegnale il valore 100. Stampa il valore della variabile in console.
+
+const number = 100; //number
+
+console.log(number)
+
+
+//ESERCIZIO 2
+//Dichiara una costante chiamata PI e assegnale il valore di 3.14. Stampa il valore della costante in console.
+
+
+
+
+//ESERCIZIO 2/B
+//Prova ad assegnare alla costante chiamata PI il valore di 3.1416. Sai dire cosa succede?
+
+
+
+
+//ESERCIZIO 2/C
+//Dichiara una variabile chiamata radius e assegnale il valore 8.
+//Moltiplica radius x 2 e x PI e assegna il valore a una variabile di nome circle.
+//Stampa il valore della variabile circle in console.
+
+
+//ESERCIZIO 3
+//Dichiara una variabile chiamata name e
+//assegnale il valore “Carlo”.
+//Successivamente, modifica il valore della variabile in “Marco”.
+//Stampa il nuovo valore in console.
+
+
