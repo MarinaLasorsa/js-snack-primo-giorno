896bfe64cb0267217ea78c9c63628b332f23f7ac
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-03-01 15:53:39 +0100

    ADD: esercizio 2/C

--- JS/app.js
+++ JS/app.js
@@ -17,13 +17,18 @@ console.log(PI)
 //ESERCIZIO 2/B
 //Prova ad assegnare alla costante chiamata PI il valore di 3.1416. Sai dire cosa succede?
 
-PI = 3.1416; //number
+//PI = 3.1416; //number
 //E' impossibile assegnare un nuovo valore a PI essendo una costante (const). Appare errore "Uncaught TypeError: invalid assignment to const 'PI'"
 
 //ESERCIZIO 2/C
 //Dichiara una variabile chiamata radius e assegnale il valore 8.
 //Moltiplica radius x 2 e x PI e assegna il valore a una variabile di nome circle.
 //Stampa il valore della variabile circle in console.
+
+var radius = 8; //number
+var circle = radius * 2 * PI; //number
+
+console.log(circle)
 
 
 //ESERCIZIO 3
